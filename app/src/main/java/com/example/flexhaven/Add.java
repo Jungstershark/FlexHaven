ce149fd7b7018ce1cb5528a32f3b0da9d81fb79f
Jung Yi <ongjy1503@gmail.com>
2024-04-02 16:32:34 +0800

    Added functionality for uploading image onto firebase storage and url of firebase storage into the realtime database

--- app/src/main/java/com/example/flexhaven/Add.java
+++ app/src/main/java/com/example/flexhaven/Add.java
@@ -1,42 +1,51 @@
 package com.example.flexhaven;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.flexhaven.helpers.CommonData;
 import com.example.flexhaven.helpers.Item;
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
-import com.example.flexhaven.helpers.User;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.chip.Chip;
 import com.google.android.material.chip.ChipGroup;
 import com.google.android.material.textfield.TextInputLayout;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class Add extends AppCompatActivity {
+    private ImageView imageView;
+    private Uri imageUri;
+    private boolean uploadImageStatus = false;
+
+    public String UriString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
 
+
         Button addItemNextButton = findViewById(R.id.addItemFinished);
+        imageView = findViewById(R.id.addImageButton);
 
         //all the preset categories
         String[] categories = CommonData.getInstance().getCategories();
@@ -76,7 +85,23 @@ public class Add extends AppCompatActivity {
             }
             autoCompleteTextView.setText("");
             autoCompleteTextView.clearFocus();
+
+
+
+
         });
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent();
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, 2);
+          }
+        });
+
+
         addItemNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -88,14 +113,29 @@ public class Add extends AppCompatActivity {
                 String location = locationEditText.getText().toString();
                 String itemDescription = itemDescriptionEditText.getText().toString();
                 //use private method to place values into firebase
-                saveItemDetailsToFirebase(email, itemName, selectedCategoriesList, condition, price, location, itemDescription);
+                saveItemDetailsToFirebase(email, itemName, selectedCategoriesList, condition, price, location, itemDescription, imageUri);
             }
         });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode ==2 && resultCode == RESULT_OK && data != null){
+            uploadImageStatus = true;
+
+            imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+
+        }
     }
 
 
     //method to handle new user input
-    private void saveItemDetailsToFirebase(String email, String itemName, ArrayList<String> category, String condition, String price, String location, String itemDescription){
+    private void saveItemDetailsToFirebase(String email, String itemName, ArrayList<String> category, String condition, String price, String location, String itemDescription, Uri imageUri){
+
+
         // TODO 2 - We could have a page if item description is empty
 //        if (category.isEmpty() || condition.isEmpty() || price.isEmpty() || location.isEmpty() || itemDescription.isEmpty()) {
 //            // if any field is empty, go to the signup error page
@@ -107,26 +147,74 @@ public class Add extends AppCompatActivity {
         FirebaseDatabase database = FirebaseDatabase.getInstance("https://infosys-37941-default-rtdb.asia-southeast1.firebasedatabase.app/");
         DatabaseReference myRef = database.getReference("Items");
 
+        StorageReference storage = FirebaseStorage.getInstance().getReference();
+
         // create a unique ID for each user
         String userId = myRef.push().getKey();
 
-        // create a user object or use a HashMap to organize user details
-        Item item = new Item(email, itemName, category, condition, price, location, itemDescription);
+        if (uploadImageStatus && imageUri!=null){
+            final StorageReference fileRef = storage.child(System.currentTimeMillis() + "." + getFileExtension(imageUri));
+            fileRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            UriString = uri.toString();
+                            Item item = new Item(email, itemName, category, condition, price, location, itemDescription, UriString);
 
-        // save the user details under their unique ID
-        if (userId != null) {
-            myRef.child(userId).setValue(item).addOnCompleteListener(task -> {
-                if (task.isSuccessful()) {
-                    //just put the user into the FYP, no need to login again!
-                    Intent newActivity = new Intent(getApplicationContext(), Profile.class);
-                    startActivity(newActivity);
+                            // save the user details under their unique ID
+                            if (userId != null) {
+                                myRef.child(userId).setValue(item).addOnCompleteListener(task -> {
+                                    if (task.isSuccessful()) {
+                                        //just put the user into the FYP, no need to login again!
+                                        Intent newActivity = new Intent(getApplicationContext(), Profile.class);
+                                        startActivity(newActivity);
+                                    }
+                    //                else {
+                    //                    //go to sign up error page, because firebase isn't working.
+                    //                    Intent newActivity = new Intent(getApplicationContext(), SignUpError.class);
+                    //                    startActivity(newActivity);
+                    //                }
+                                                    });
+                                                }
+                        }
+                    });
                 }
+            });
+        }
+        else {
+            Item item = new Item(email, itemName, category, condition, price, location, itemDescription, "");
+
+            // save the user details under their unique ID
+            if (userId != null) {
+                myRef.child(userId).setValue(item).addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        //just put the user into the FYP, no need to login again!
+                        Intent newActivity = new Intent(getApplicationContext(), Profile.class);
+                        startActivity(newActivity);
+                    }
 //                else {
 //                    //go to sign up error page, because firebase isn't working.
 //                    Intent newActivity = new Intent(getApplicationContext(), SignUpError.class);
 //                    startActivity(newActivity);
 //                }
-            });
+                });
+            }
         }
+
+
+
+
+
     }
-}
+
+    private String getFileExtension(Uri mUri){
+
+        ContentResolver cr = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cr.getType(mUri));
+
+    }
+
+        }
