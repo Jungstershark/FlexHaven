4c3660e2eeaa688820e546039990cb906a936370
Jung Yi <ongjy1503@gmail.com>
2024-04-08 05:27:05 +0800

    Revamped the layout to a recycler view, dynamic input of items from the database. Fixed the bottom navigation bar. Accounted for both of these changes in the activity_profile.xml file as well as the Profile.java. The only thing left is limiting the recycler view items to just the current user email items.

--- app/src/main/java/com/example/flexhaven/Profile.java
+++ app/src/main/java/com/example/flexhaven/Profile.java
@@ -4,13 +4,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.flexhaven.helpers.CommonData;
+import com.example.flexhaven.helpers.Item;
+import com.example.flexhaven.helpers.ListingAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class Profile extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private ListingAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,11 +41,62 @@ public class Profile extends AppCompatActivity {
         //TODO implement dynamic display with recyclerView here!
         Button displayListingsButton = findViewById(R.id.DisplayListingsProfile);
 
+        recyclerView = findViewById(R.id.profileRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new ListingAdapter(this, new ArrayList<>());
+        recyclerView.setAdapter(adapter);
+
+        generateItems();
+
         addItemButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(Profile.this,Add.class));
             }
         });
+
+
+        ImageButton homeButton = findViewById(R.id.ProfileMenuFYPButton);
+        ImageButton searchButton = findViewById(R.id.ProfileMenuSearchButton);
+
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), FYP.class));
+            }
+        });
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Search.class));
+            }
+        });
+    }
+
+    private void generateItems() {
+        DatabaseReference itemsRef = FirebaseDatabase.getInstance("https://infosys-37941-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("Items");
+
+        itemsRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                ArrayList<Item> allItems = new ArrayList<>();
+                for (DataSnapshot itemSnapshot : dataSnapshot.getChildren()) {
+                    Item item = itemSnapshot.getValue(Item.class);
+                    if (item != null) {
+                        allItems.add(item);
+                    }
+                }
+                runOnUiThread(() -> refreshRecyclerViewWithNewData(allItems));
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Handle onCancelled event
+            }
+        });
+    }
+    public void refreshRecyclerViewWithNewData(ArrayList<Item> newData) {
+        adapter.updateItems(newData);
     }
 }
