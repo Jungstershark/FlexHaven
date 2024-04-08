095653b6a7ce64e6dce152c2a041e258428b48a8
Jung Yi <ongjy1503@gmail.com>
2024-04-09 06:28:09 +0800

    Updated and added functionality for each of the image buttons in the FYP page. They not lead to a page

--- app/src/main/java/com/example/flexhaven/FYP.java
+++ app/src/main/java/com/example/flexhaven/FYP.java
@@ -3,6 +3,7 @@ package com.example.flexhaven;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +15,52 @@ public class FYP extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fyp);
+
+        ImageButton featuredButton = findViewById(R.id.featuredButton);
+        featuredButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent newActivity = new Intent(getApplicationContext(),ItemDescription.class);
+                startActivity(newActivity);
+            }
+        });
+
+        ImageButton womanButton = findViewById(R.id.womenButton);
+        womanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String category = "Woman";
+
+                Intent newActivity = new Intent(getApplicationContext(),Listing.class);
+                newActivity.putExtra("CATEGORY",category);
+                startActivity(newActivity);
+            }
+        });
+
+        ImageButton menButton = findViewById(R.id.menButton);
+        menButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String category = "Shorts";
+
+                Intent newActivity = new Intent(getApplicationContext(),Listing.class);
+                newActivity.putExtra("CATEGORY",category);
+                startActivity(newActivity);
+            }
+        });
+        ImageButton accessoriesButton = findViewById(R.id.accessoriesButton);
+        accessoriesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String category = "Jeans";
+
+                Intent newActivity = new Intent(getApplicationContext(),Listing.class);
+                newActivity.putExtra("CATEGORY",category);
+                startActivity(newActivity);
+            }
+        });
+
+
 
         ImageButton profileButton = findViewById(R.id.profile);
         ImageButton searchButton = findViewById(R.id.search);
