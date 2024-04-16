f2eebbe51531052e215b771a8eb63a16b1d49a9b
Jung Yi <ongjy1503@gmail.com>
2024-04-17 01:45:18 +0800

    Implemented additional functionality to update the seller points and buyer points and their tiers within the profile page. Added additional filters to not allow items that have been rented out to be displayed on the lisitngs page recycler view. Implemented add wallet value functionality which allow users to specify an integer value to be added to their in app wallet, it then updates this value to the database. Implemented toggle view for the userlistings, userRented out and userRentals.

--- app/src/main/java/com/example/flexhaven/Profile.java
+++ app/src/main/java/com/example/flexhaven/Profile.java
@@ -4,12 +4,16 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -19,10 +23,12 @@ import com.example.flexhaven.helpers.CommonData;
 import com.example.flexhaven.helpers.Item;
 import com.example.flexhaven.helpers.ListingAdapter;
 import com.example.flexhaven.helpers.User;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import org.checkerframework.checker.units.qual.Current;
@@ -34,6 +40,7 @@ public class  Profile extends AppCompatActivity {
     private RecyclerView recyclerView;
     private ListingAdapter adapter;
     private Drawable tierIcon;
+    private int amountToAdd;
     @SuppressLint("UseCompatLoadingForDrawables")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,11 +74,6 @@ public class  Profile extends AppCompatActivity {
 
         // Get user Tier and user Points from currentUser user object
         TextView userTierPoints = findViewById(R.id.userTierPointsTextView);
-        int currentUserPoints = currentUser.userPoints;
-        if (currentUser!=null){
-            userTierPoints.setText(currentUserPoints + " Points");
-        }
-
         if (currentUser != null) {
             currentUser.computeTier();
             switch (currentUser.userTier) {
@@ -90,14 +92,73 @@ public class  Profile extends AppCompatActivity {
                 default:
                     tierIcon = getResources().getDrawable(R.drawable.tier_bronze_icon);
             }
-            userTierPoints.setCompoundDrawablesRelativeWithIntrinsicBounds(tierIcon, null, null, null);
+            userTierPoints.setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, tierIcon, null);
         }
+        int currentUserPoints = currentUser.userPoints;
+        if (currentUser!=null){
+            userTierPoints.setText("Buyer: " + currentUserPoints + " Points" + "  |  " + currentUser.userTier);
+        }
+
+        // Get seller Tier and seller Points from currentUser user object
+        TextView sellerTierPoints = findViewById(R.id.sellerTierPointsTextView);
+        if (currentUser != null) {
+            currentUser.computeSellerTier();
+            switch (currentUser.sellerTier) {
+                case "Bronze":
+                    tierIcon = getResources().getDrawable(R.drawable.tier_bronze_icon);
+                    break;
+                case "Silver":
+                    tierIcon = getResources().getDrawable(R.drawable.tier_silver_icon);
+                    break;
+                case "Gold":
+                    tierIcon = getResources().getDrawable(R.drawable.tier_gold_icon);
+                    break;
+                case "Platinum":
+                    tierIcon = getResources().getDrawable(R.drawable.tier_platinum_icon);
+                    break;
+                default:
+                    tierIcon = getResources().getDrawable(R.drawable.tier_bronze_icon);
+            }
+            sellerTierPoints.setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, tierIcon, null);
+        }
+        int sellerUserPoints = currentUser.sellerPoints;
+        if (currentUser!=null){
+            sellerTierPoints.setText("Seller: " + sellerUserPoints + " Points" + "  |  " + currentUser.sellerTier);
+        }
+
+        TextView walletValueTextView = findViewById(R.id.WalletValue);
+        walletValueTextView.setText("$" + currentUser.userWallet);
+
 
         // Get user email from currentUser user object
         TextView userEmail = findViewById(R.id.userEmailTextView);
         if (currentUser!=null){
             userEmail.setText(currentUser.email);
         }
+
+        Button addWalletValue = findViewById(R.id.AddWalletValue);
+        addWalletValue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText amountToAddEditText = findViewById(R.id.AmountToAddInput);
+                String amountToAddString = amountToAddEditText.getText().toString().trim();
+                try {
+                    int amountToAdd = Integer.parseInt(amountToAddString);
+                    TextView walletValueTextView = findViewById(R.id.WalletValue);
+                    int totalWalletValue = amountToAdd + currentUser.userWallet;
+                    currentUser.userWallet = totalWalletValue;
+                    walletValueTextView.setText("$" + currentUser.userWallet);
+
+                    // Update Database
+                    updateUserWallet(totalWalletValue, currentUser);
+
+                    // Show a toast message indicating success
+                    Toast.makeText(Profile.this, "Wallet value updated successfully!", Toast.LENGTH_SHORT).show();
+                } catch (NumberFormatException e) {
+                    showErrorMessage("Type a valid Integer Value");
+                }
+            }
+        });
 
         Button addItemButton = findViewById(R.id.AddItemProfile);
 
@@ -106,7 +167,7 @@ public class  Profile extends AppCompatActivity {
         adapter = new ListingAdapter(this, new ArrayList<>());
         recyclerView.setAdapter(adapter);
 
-        generateItems();
+        generateItems(0);
 
         addItemButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -115,6 +176,39 @@ public class  Profile extends AppCompatActivity {
             }
         });
 
+        Button userListings = findViewById(R.id.userListings);
+        Button userRentedOut = findViewById(R.id.userRentedOut);
+        Button userRentals = findViewById(R.id.userRentals);
+
+        userListings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userListings.setBackgroundColor(getResources().getColor(R.color.clicked_button_color));
+                userRentedOut.setBackgroundColor(getResources().getColor(R.color.default_button_color));
+                userRentals.setBackgroundColor(getResources().getColor(R.color.default_button_color));
+                generateItems(0);
+            }
+        });
+
+        userRentedOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userListings.setBackgroundColor(getResources().getColor(R.color.default_button_color));
+                userRentedOut.setBackgroundColor(getResources().getColor(R.color.clicked_button_color));
+                userRentals.setBackgroundColor(getResources().getColor(R.color.default_button_color));
+                generateItems(1);
+            }
+        });
+
+        userRentals.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userListings.setBackgroundColor(getResources().getColor(R.color.default_button_color));
+                userRentedOut.setBackgroundColor(getResources().getColor(R.color.default_button_color));
+                userRentals.setBackgroundColor(getResources().getColor(R.color.clicked_button_color));
+                generateItems(2);
+            }
+        });
 
         ImageButton homeButton = findViewById(R.id.ProfileMenuFYPButton);
         ImageButton searchButton = findViewById(R.id.ProfileMenuSearchButton);
@@ -143,7 +237,7 @@ public class  Profile extends AppCompatActivity {
         });
     }
 
-    private void generateItems() {
+    private void generateItems(int status) {
         // Get the reference to the Firebase Realtime Database
         DatabaseReference itemsRef = FirebaseDatabase.getInstance("https://infosys-37941-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("Items");
 
@@ -153,16 +247,36 @@ public class  Profile extends AppCompatActivity {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 ArrayList<Item> userItems = new ArrayList<>();
                 String currentUserEmail = CommonData.getInstance().getCurrentUser().email;
+                String currentUserKey = CommonData.getInstance().getCurrentUser().userKey;
 
                 // Iterate through each item in the dataSnapshot
                 for (DataSnapshot itemSnapshot : dataSnapshot.getChildren()) {
                     // Get the item object
                     Item item = itemSnapshot.getValue(Item.class);
+                    String itemUserkey = item.getOwner().username + "_" + item.getOwner().email;
 
                     // Check if the item owner's email matches the current user's email
-                    if (item != null && item.getOwner().email.equals(currentUserEmail)) {
-                        // Add the item to the list of user items
-                        userItems.add(item);
+                    if (item != null) {
+                        switch (status) {
+                            case 0:
+                                // Add the item to the list if the user is the owner and rental status is 0
+                                if (itemUserkey.equals(currentUserKey) && item.getRentalStatus().rentalStatus==0) {
+                                    userItems.add(item);
+                                }
+                                break;
+                            case 1:
+                                // Add the item to the list if the user is the owner and rental status is 1
+                                if (itemUserkey.equals(currentUserKey) && item.getRentalStatus().rentalStatus == 1) {
+                                    userItems.add(item);
+                                }
+                                break;
+                            case 2:
+                                // Add the item to the list if the rental status is 1 and renter email is the current user's email
+                                if (item.getRentalStatus().rentalStatus== 1 && item.getRentalStatus().itemRenterEmail.equals(currentUserEmail)) {
+                                    userItems.add(item);
+                                }
+                                break;
+                        }
                     }
                 }
 
@@ -179,5 +293,32 @@ public class  Profile extends AppCompatActivity {
 
     public void refreshRecyclerViewWithNewData(ArrayList<Item> newData) {
         adapter.updateItems(newData);
+    }
+    private void updateUserWallet(int totalWalletValue, User currentUser) {
+        DatabaseReference userRef = FirebaseDatabase.getInstance("https://infosys-37941-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("Users");
+        Query query = userRef.orderByChild("userKey").equalTo(currentUser.userKey);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
+                        userRef.child(userSnapshot.getKey()).child("userWallet").setValue(totalWalletValue);
+                        currentUser.userWallet = totalWalletValue;
+                        Log.d("UpdateUserWallet", "User wallet updated successfully");
+                    }
+                } else {
+                    Log.d("UpdateUserWallet", "User data not found in the database");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e("UpdateUserWallet", "Database error: " + databaseError.getMessage());
+                Toast.makeText(Profile.this, "Database error: " + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    private void showErrorMessage(String message) {
+        Toast.makeText(Profile.this, message, Toast.LENGTH_SHORT).show();
     }
 }
